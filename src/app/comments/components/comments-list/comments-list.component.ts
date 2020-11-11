224ff413237af8a4b6fe49f77451fc4296c485c6
Herman Andres <heanfig@gmail.com>
2020-11-10 20:27:53 -0500

    Comments Module: Add Search

--- src/app/comments/components/comments-list/comments-list.component.ts
+++ src/app/comments/components/comments-list/comments-list.component.ts
@@ -1,6 +1,7 @@
 import { Comment } from '../../models/comment.model';
 import { Component, OnInit } from '@angular/core';
 import { CommentsService } from '../../services/comments.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-comments-list',
@@ -10,15 +11,29 @@ import { CommentsService } from '../../services/comments.service';
 export class CommentsListComponent implements OnInit {
 
   public commentsData: Comment[];
+  public commentsDataTemp: Comment[];
+  public searchName = new FormControl('');
 
   constructor(
     private commentService: CommentsService
   ) { }
 
   ngOnInit() {
+
+    this.searchName.valueChanges.subscribe((key:string) =>{
+      if(key === ''){
+        this.commentsData = this.commentsDataTemp;
+      } else {
+        this.commentsData = this.commentsData.filter( item => 
+          item.name.includes(key) || item.email.includes(key) || item.website.includes(key)
+        );
+      }
+    })
+
     this.commentService.getComments().subscribe(
       (response: Comment[]) => {
         this.commentsData = response;
+        this.commentsDataTemp = response;
       }
     );
   }
